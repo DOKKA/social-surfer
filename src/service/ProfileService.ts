2bea36950c077bfa47e7ad7e6fd3829dfc134c7a
Kevin Grieger <scuffmark@gmail.com>
2022-03-28 23:39:32 -0500

    adding more properties to profileservice

--- src/service/ProfileService.ts
+++ src/service/ProfileService.ts
@@ -2,17 +2,25 @@ import cors_fetch from "../util/CorsFetch";
 
 
 
-
+//type NullableString = string|null;
 
 export default class ProfileService{
 
     profileURL:string;
     image: string|null;
-    summary: string | null;
+    summary: string| null;
+    followers: string | null;
+    following: string | null;
+    inbox: string | null;
+    outbox: string | null;
     constructor(profileURL:string){
         this.profileURL = profileURL;
         this.image = null;
         this.summary = null;
+        this.followers = null;
+        this.following = null;
+        this.inbox = null;
+        this.outbox = null;
     }
 
     async getProfileJSON(){
@@ -22,5 +30,9 @@ export default class ProfileService{
 
         this.image = json.icon.url;
         this.summary = json.summary;
+        this.followers = json.followers;
+        this.following = json.following;
+        this.inbox = json.inbox;
+        this.outbox = json.outbox;
     }
 }
